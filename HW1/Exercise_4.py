5bdce450c20ac160b0d748245c27eb2d7547fe39
RandallMencias <randallmencias@gmail.com>
2022-09-24 14:20:44 -0500

    Clock Decorator added

--- HW1/Exercise_4.py
+++ HW1/Exercise_4.py
@@ -1,27 +1,25 @@
 import time
 
-def sum1(n):
-    t0 = time.time()
-    total = 0
-    for x in range(1,n+1):
-        total += x
-    return total, time.time() - t0
-
-# Python decorators
-# Hint: Google "execution time with decorators in Python"
+def clock(func):
+    def inner(*args, **kwargs):
+        start = time.time()
+        answer = func(*args, **kwargs)
+        print("Execution Time:" + str(time.time()-start) + "ms\n Total Sum = " + str(answer))
+        return answer
+    return inner
 
 
-def sum1(n):
-    """
-    Sum of numbers between 1 and n
-    inputs:
-        n (int): last integer to add
-    outputs:
-        total (int): total sum
-    """
 
-    total = 0
-    # range goes only until (n+1)-1
-    for x in range(1,n+1):
-        total += x
-    return total
+@clock
+def sum2(n):
+        total = n * (n + 1) // 2
+        return total
+
+
+
+sum2(5)
+
+
+
+
+
