79a75d68ef729deca495b1ef1310f8bb272fd210
RandallMencias <randallmencias@gmail.com>
2022-09-24 14:44:25 -0500

    Summation methods Testing

--- HW1/Exercise_4.py
+++ HW1/Exercise_4.py
@@ -1,13 +1,21 @@
 import time
 
 def clock(func):
-    def inner(*args, **kwargs):
+    def inner(args):
         start = time.time()
-        answer = func(*args, **kwargs)
-        print("Execution Time:" + str(time.time()-start) + "ms\n Total Sum = " + str(answer))
+        answer = func(args)
+        end = time.time()
+        print("Execution Time:" + str(end-start) + "ms\nTotal Sum = " + str(answer))
         return answer
     return inner
 
+
+@clock
+def sum1(n):
+    total = 0
+    for i in range(1, n+1):
+        total += i
+    return total
 
 
 @clock
@@ -16,10 +24,18 @@ def sum2(n):
         return total
 
 
+def main(n):
+    print("n = " +str(n)+"\nLoop:")
+    sum1(n)
+    print("Gaussian addition:")
+    sum2(n)
 
-sum2(5)
-
-
-
-
-
+if __name__ == "__main__":
+    print("N Test")
+    main(5)
+    main(8)
+    main(103)
+    main(527)
+    print("N timings")
+    for i in range(0,10):
+        main(1*(pow(10,i)))
