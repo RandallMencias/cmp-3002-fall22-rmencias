6702199c74b2ab85fce06517ad944552174d6653
RandallMencias <randallmencias@gmail.com>
2022-10-04 00:23:06 -0500

    Added Plot

--- HW1/Exercise_4.py
+++ HW1/Exercise_4.py
@@ -1,34 +1,41 @@
-import time
+from time import time
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+from pandas import DataFrame
+
 
 def clock(func):
     def inner(args):
-        start = time.time()
+        start = time() * 10 ** 9
         answer = func(args)
-        end = time.time()
-        print("Execution Time:" + str(end-start) + "s\nTotal Sum = " + str(answer)+"\n")
-        return answer
+        end = time() * 10 ** 9
+        print("Execution Time:" + str(end - start) + "s\nTotal Sum = " + str(answer) + "\n")
+        return answer, end - start
+
     return inner
 
 
 @clock
 def sum1(n):
     total = 0
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         total += i
     return total
 
 
 @clock
 def sum2(n):
-        total = n * (n + 1) // 2
-        return total
+    total = n * (n + 1) // 2
+    return total
 
 
 def main(n):
-    print("n = " +str(n)+"\nLoop:")
+    print("n = " + str(n) + "\nLoop:")
     sum1(n)
     print("Gaussian addition:")
     sum2(n)
+
 
 if __name__ == "__main__":
     print("N Test")
@@ -37,5 +44,42 @@ if __name__ == "__main__":
     main(103)
     main(527)
     print("N timings")
-    for i in range(0,10):
-        main(1*(pow(10,i)))
+    runtime1 = [sum1(x)[1] for x in [1, 10, 100, 1000, 10000,100000,1000000,10000000,100000000,1000000000]]
+    runtime2 = [sum2(x)[1] for x in [1, 10, 100, 1000, 10000,100000,1000000,10000000,100000000,1000000000]]
+    x = [1, 10, 100, 1000, 10000, 100000, 1000000,10000000,100000000,1000000000]
+    print(runtime1)
+    print(runtime2)
+    plt.plot(x, runtime1, label= "Sum1")
+    plt.plot(x, runtime2, label="Sum2")
+    plt.xlabel('n')
+    plt.ylabel('runtime')
+    plt.title("Loop Vs Gaussian Sum")
+    plt.grid()
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
