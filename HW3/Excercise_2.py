bffc99e524a778873805460bdaa7bcf53304b427
RandallMencias <randallmencias@gmail.com>
2022-11-15 00:54:04 -0500

    Excercise_1

---
+++ HW3/Excercise_2.py
@@ -0,0 +1,22 @@
+def quicksort(list):
+    if len(list) <= 1:
+        return list
+    pivot = list.pop()
+    l1 = []
+    l2 = []
+    for i in list:
+        if i <= pivot:
+            l1.append(i)
+        else:
+            l2.append(i)
+
+    return quicksort(l1) + [pivot] + quicksort(l2)
+
+
+def main():
+    list1 = [4, 5, 3, 1, 10, 4, 6, 8]
+    print(quicksort(list1))
+
+
+if __name__ == "__main__":
+    main()
